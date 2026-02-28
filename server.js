8a6049385d4c23964e1cde69f4e5d4cc9f0dd733
hlnjs <elizabethhelen25@gmail.com>
2026-02-28 09:10:29 +0530

    added dotenv

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
+const dotenv = require("dotenv");
 
+dotenv.config();
 const app = express();
 
 app.use(cors());
